0a1d1210203b9b93d16792abc983a32f18d4134c
Romain Beka <105503611+romainbeka@users.noreply.github.com>
2022-05-13 18:06:44 +0200

    Add files via upload

---
+++ styles-switcher.dev.js
@@ -0,0 +1,83 @@
+"use strict";
+
+/* toggle style switcher */
+var styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", function () {
+  document.querySelector(".style-switcher").classList.toggle("open");
+}); // hide style - switcher on scroll
+
+window.addEventListener("scroll", function () {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+/* theme colors */
+
+var alternateStyle = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+  localStorage.setItem("color", color);
+  changeColor();
+}
+
+function changeColor() {
+  alternateStyle.forEach(function (style) {
+    if (localStorage.getItem("color") === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+} // checking if 'color' key exists
+
+
+if (localStorage.getItem("color") !== null) {
+  changeColor();
+}
+/* theme light and dark mode */
+
+
+var dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+
+  updateIcon();
+});
+
+function themeMode() {
+  // checking if 'theme' key exists
+  if (localStorage.getItem("theme") !== null) {
+    if (localStorage.getItem("theme") === "light") {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  }
+
+  updateIcon();
+}
+
+themeMode();
+
+function updateIcon() {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.remove("fa-moon");
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.remove("fa-sun");
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+} // window.addEventListener("load", () =>{
+//     if(document.body.classList.contains("dark")){
+//         dayNight.querySelector("i").classList.add("fa-sun");
+//     }
+//     else{
+//         dayNight.querySelector("i").classList.add("fa-moon");
+//     }
+// })
